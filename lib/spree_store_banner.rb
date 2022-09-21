c338b16d27c5e2fb7c577221cc317c299d0d1332
Vishal <vishal@bluebash.co>
2022-09-21 13:16:02 +0530

    Initial Commit

---
+++ lib/spree_store_banner.rb
@@ -0,0 +1,4 @@
+require 'spree_core'
+require 'spree_extension'
+require 'spree_store_banner/engine'
+require 'spree_store_banner/version'
---
+++ lib/spree_store_banner/engine.rb
@@ -0,0 +1,24 @@
+module SpreeStoreBanner
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_store_banner'
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    initializer 'spree_store_banner.environment', before: :load_config_initializers do |_app|
+      SpreeStoreBanner::Config = SpreeStoreBanner::Configuration.new
+    end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare(&method(:activate).to_proc)
+  end
+end
