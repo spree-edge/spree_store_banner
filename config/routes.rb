bb27d72f4d5907cd13d97e5f6a3f93c9f02c08f1
Vishal <vishal@bluebash.co>
2022-10-19 10:42:41 +0530

    Update the routes and added the banner column in configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
+  namespace :admin, path: Spree.admin_path do
+    resources :banners
+  end
 end
