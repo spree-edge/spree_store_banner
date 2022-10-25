6b5e764d7f5807219f12b074916609ce6124f3a4
Vishal <vishal@bluebash.co>
2022-10-25 17:16:23 +0530

    Add the new field for the banner name in the table

--- db/migrate/20221014115853_create_spree_banners.rb
+++ db/migrate/20221014115853_create_spree_banners.rb
@@ -1,6 +1,7 @@
 class CreateSpreeBanners < ActiveRecord::Migration[6.1]
   def change
     create_table :spree_banners do |t|
+      t.string :name
       t.text :content
       t.string :color
       t.boolean :published, default: false
