d79183d958fe15d13881abfb246d40a72b5ac431
Shrey <shrey.gupta@bluebash.co>
2024-08-21 16:41:42 +0530

    Minor Fix

--- app/helpers/spree/banner_helper.rb
+++ app/helpers/spree/banner_helper.rb
@@ -3,6 +3,8 @@
 module Spree
   module BannerHelper
     def banner
+      return unless current_store.present?
+
       banners = current_store.banners.where(published: true)
       banners.each do |banner|
         @result = banner
